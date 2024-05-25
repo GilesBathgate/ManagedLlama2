dd34e1945f27779f6d12a3b3c1b8c9fee9e0f3f7
Giles Bathgate <giles.bathgate@gmail.com>
2024-05-25 09:24:00 +0100

    Add example Setup utility

---
+++ examples/Setup/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Example.Setup;
+
+using libLlama2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: Program.exe <output-model.bin> <output-tokenizer.bin>");
+            return;
+        }
+
+        var modelPath = args[0];
+        var tokenizerPath = args[1];
+
+        var modelConverter = new ModelConverter(download: true);
+        modelConverter.Convert(modelPath);
+
+        var tokenizerConverter = new TokenizerConverter(download: true);
+        tokenizerConverter.Convert(tokenizerPath);
+
+        File.Delete("pytorch_model.pt");
+        File.Delete("config.json");
+        File.Delete("tokenizer.spm");
+    }
+}
