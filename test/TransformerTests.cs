d629dfc0d65f139bc5aa13ed9d73f89e6d917a7f
Giles Bathgate <giles.bathgate@gmail.com>
2024-05-25 09:25:00 +0100

    Add more tokeniser tests

--- test/TransformerTests.cs
+++ test/TransformerTests.cs
@@ -22,16 +22,41 @@ public class TransformerTests
         Assert.Equal(expected, actual);
     }
 
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { "You are a helpful assistant", new[] { 1, 887, 526, 263, 8444, 20255, 2 } };
+        yield return new object[] { "<<SYS>>", new[] { 1, 3532, 14816, 29903, 6778, 2 } };
+        yield return new object[] { "[/INST]", new[] { 1, 518, 29914, 25580, 29962, 2 } };
+        yield return new object[] { "\n", new[] { 1, 29871, 13, 2 } };
+    }
+
     [Theory(Skip = "Requires tokenizer.bin")]
-    [InlineData("You are a helpful assistant", new[] { 1, 887, 526, 263, 8444, 20255, 2 })]
-    [InlineData("<<SYS>>", new[] { 1, 3532, 14816, 29903, 6778, 2 })]
-    [InlineData("[/INST]", new[] { 1, 518, 29914, 25580, 29962, 2 })]
-    [InlineData("\n", new[] { 1, 29871, 13, 2 })]
-    public void Test_Tokenizer(string value, int[] expected)
+    [MemberData(nameof(TestData))]
+    public void Test_TokenizerEncode(string value, int[] expected)
     {
         var tokenizer = new Tokenizer("tokenizer.bin", 32000);
 
         var actual = tokenizer.Encode(value, true, true);
+
+        Assert.Equal(expected, actual);
+
+    }
+
+    [Theory(Skip = "Requires tokenizer.bin")]
+    [MemberData(nameof(TestData))]
+    public void Test_TokenizerDecode(string test, int[] value)
+    {
+        var expected = $"\n<s>\n{test}\n</s>\n";
+        var tokenizer = new Tokenizer("tokenizer.bin", 32000);
+
+        var sb = new StringBuilder();
+        var prev = 0;
+        foreach (var token in value)
+        {
+            sb.Append(tokenizer.Decode(prev, token));
+            prev = token;
+        }
+        var actual = sb.ToString();
 
         Assert.Equal(expected, actual);
 
